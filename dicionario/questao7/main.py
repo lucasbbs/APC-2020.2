b80624d25c7a366143b02b7307eacc7c124b4dbd
lucasbbs <45081920+lucasbbs@users.noreply.github.com>
2021-05-21 17:54:20 -0300

    Update main.py

--- dicionario/questao7/main.py
+++ dicionario/questao7/main.py
@@ -10,3 +10,30 @@ for palavra in frase:
     for dicts in lista:
       if palavra == list(dicts.keys())[0]:nova_lista.append(dicts[list(dicts.keys())[0]])
 print(" ".join(nova_lista))
+
+tradutor = {}
+n = int(input())
+for i in range(n):
+    traduzir = input().split()
+    tradutor[traduzir[0]] = ' '.join(traduzir[1:])
+texto = input().split()
+resp = ""
+for palavra in texto:
+    resp += tradutor[palavra]
+    resp += ' '
+print(resp)
+
+# versao pythonica
+#print(' '.join([traduzir[palavra] for palavra in input().split()])
+
+
+# Solucao alternativa
+#
+# dict = {}
+# for _ in range(int(input())):
+#     a, b = input().split(' ', 1)
+#     dict[a] = b
+# frase = input().split()
+# for palavra in frase:
+#     print(dict[palavra], end=' ')
+# print()
