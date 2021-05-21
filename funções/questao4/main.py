a8705a29e4409a878306f737d4c4c981f2cadaf0
lucasbbs <45081920+lucasbbs@users.noreply.github.com>
2021-05-21 11:07:45 -0300

    Create main.py

---
+++ funções/questao4/main.py
@@ -0,0 +1,14 @@
+decimal1 = int(input())
+decimal2 = int(input())
+decimal3 = int(input())
+decimal4 = int(input())
+decimal5 = int(input())
+
+def binario(decimal):
+    return bin(decimal)
+
+print(binario(decimal1))
+print(binario(decimal2))
+print(binario(decimal3))
+print(binario(decimal4))
+print(binario(decimal5))
