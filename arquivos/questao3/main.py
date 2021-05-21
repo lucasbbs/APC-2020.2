ef188b6be46eb5a652d0938ae1bece674d570b63
lucasbbs <45081920+lucasbbs@users.noreply.github.com>
2021-05-21 18:00:48 -0300

    Create main.py

---
+++ arquivos/questao3/main.py
@@ -0,0 +1,10 @@
+list_of_lists = []
+
+with open(input()) as file:
+  for line in file:
+      stripped_line = line.strip()
+      line_list = stripped_line.split()
+      list_of_lists.append(line_list)
+    
+lines = [line[0].split(",") for line in list_of_lists]
+[[print(line[-1]) for i,word in enumerate(line) if i == len(line)-1] for line in lines]
