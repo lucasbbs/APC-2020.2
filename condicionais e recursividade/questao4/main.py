720ace5d7785f4d4640ccb9e586bbe6272f00484
lucasbbs <45081920+lucasbbs@users.noreply.github.com>
2021-05-21 13:08:19 -0300

    Create main.py

---
+++ condicionais e recursividade/questao4/main.py
@@ -0,0 +1,9 @@
+def area(arg1, arg2, string):
+    if string == "triangulo":
+        print(f'O {string} tem {arg1*arg2/2:.0f} de area')
+    if string == "losango":
+        print(f'O {string} tem {arg1*arg2/2:.0f} de area')
+    if string == "retangulo":
+        print(f'O {string} tem {arg1*arg2:.0f} de area')
+    if string == "circulo":
+        print(f'O {string} tem {arg1**2*arg2:.0f} de area')
