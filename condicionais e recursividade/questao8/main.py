ae45d1d280d50e6a92f87057d320ca6409e66689
lucasbbs <45081920+lucasbbs@users.noreply.github.com>
2021-05-21 13:37:16 -0300

    Create main.py

---
+++ condicionais e recursividade/questao8/main.py
@@ -0,0 +1,14 @@
+def sonho(n):
+    while True:
+        n+=1
+        print(n)
+        m = int(input())
+        if m%2 ==0:
+          break
+          
+# def sonho(n):
+#     n += 1
+#     print(n)
+#     x = int(input())
+#     if x % 2 == 1:
+#         sonho(n)
