f36a790222dee5c4ec71edb9834bbfb09f46b197
lucasbbs <45081920+lucasbbs@users.noreply.github.com>
2021-05-21 15:18:20 -0300

    Update main.py

--- funções com resultados/questao7/main.py
+++ funções com resultados/questao7/main.py
@@ -8,7 +8,7 @@ def binary(num, string):
     string = string + str(num % 2)
     return binary(num // 2, string)
 
-  
+# Para resolver o problema, deve-se realizar a conversão do número da entrada x para sua representação binária. Podemos fazer isso criando uma função recursiva, em que cada chamada deve gerar um bit de sua representação, bastando pegar o resto da divisão de x por 2 e contabilizar no resultado final (no comando return) a presença do bit 1. A chamada recursiva deve ter como argumento o quociente de x÷2 e ter como critério de parada o caso em que o x é igual a zero.
 # def count_1s(x):
 #     if x == 0:
 #         return 0
