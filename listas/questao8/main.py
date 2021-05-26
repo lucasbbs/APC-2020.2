ae32a27db021109fc441234ceefd876e1e91b789
lucasbbs <45081920+lucasbbs@users.noreply.github.com>
2021-05-26 16:37:56 -0300

    Update main.py

--- listas/questao8/main.py
+++ listas/questao8/main.py
@@ -29,3 +29,23 @@ n = int(input())
 nums = input().split(' ')
 nums = [int(x) for x in nums]
 print(solve(nums))
+
+
+'''
+Existem 3 formas de resolver esse problema.
+
+A primeira, e mais óbvia, é tentar todas as somas possíveis entre 2 números do vetor, e se alguma der 42, respondemos sim. Se testamos todas as somas e nenhuma deu 42, a resposta é nao. Essa forma é simples de ser implementada, mas é pouco eficiente.
+
+Uma segunda alternativa é ordenar o vetor. Feito isso, podemos utilizar uma técnica chamada two pointers para descobrir se é possível somar 2 números até 42 da seguinte maneira:
+
+1. Ordenar o vetor
+
+2. Inicializar um ponteiro L=0 e R=n−1 (primeiro e último elementos do vetor
+
+3. Enquanto o ponteiro L for menor do que R, analisamos a soma atual. Se ela for 42, retornamos sim. Se ela for menor do que 42, então precisamos de números maiores na soma, logo incrementamos L uma posição. Se a soma for maior que 42, precisamos de números menores, logo decrementamos R uma posição.
+
+4. Se L==R e não achamos uma soma que dá 42, podemos responder nao.
+
+
+
+A terceira maneira de responder a questão envolve usar um mapa para armazenar quais números foram vistos. Quando você aprender sobre dicionários em Python pode tentar resolver essa questão novamente usando eles :D '''
